feecb9d4816a6e153da1f37acfcb4a6f5d448c92
Lars Finley Martinson <larsm_eyes@hotmail.com>
2016-02-18 14:50:39 -0700

    auto-pops new bear, deletes, full stack

--- public/app.js
+++ public/app.js
@@ -3,7 +3,7 @@ var deleteBear = function(){
 	var bear = $(event.target).closest('tr');
 	
 	if (confirm("Are you sure")) {	
-	$.ajax({//method that allows us to comm with our database aka postman
+	$.ajax({
 		url: '/api/bears/' + id,
 		method: 'DELETE',
 	}).done(function(){
@@ -11,7 +11,42 @@ var deleteBear = function(){
 	})
 }
 }
+//method that allows us to comm with our database aka =postman
 
-$('.deleteBear').on('click', deleteBear);
+	
+var addBear = function(event){
+	event.preventDefault();
+	var name = $('#name').val();
+	var age = $('#age').val();
+	var gender = $('#gender').val();
+	var $table = $('#bearTable');
+	var bear = {};
+		bear.name = name;
+		bear.age = age;
+		bear.gender = gender;
+
+	$.ajax({
+		url: '/api/bears',
+		method: "POST",
+		data: bear,
+	}).done(function(data){
+		console.log("worked", data);
+
+		$table.append('<tr id=' + data._id + '>\
+                   <td>' + data.name + '</td>\
+                   <td>' + data.age + '</td>\
+                   <td>' + data.gender + '</td>\
+                   <td><button class="btn btn-danger deleteBear">Hibernate Bear</button></td>\
+                 </tr>'
+               );
+			
+			$('.deleteBear').on('click', deleteBear);
+	})
+	$('#name').val("");
+	$('#age').val("");
+	$('gender').val("");
+}
+
+$("#addBear").on("click", addBear);
 
 //public side browser, not node, using jquery and html
